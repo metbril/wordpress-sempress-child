14e40eed26453181bbe58f0e27f1cc11ffc3acac
Robert van Bregt <git@robertvanbregt.nl>
2022-02-01 20:45:13 +0100

    Replace font with system fonts

--- functions.php
+++ functions.php
@@ -9,10 +9,6 @@ function my_theme_enqueue_styles() {
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
-    wp_enqueue_style( 'sempress-child-fonts',
-        get_stylesheet_directory_uri() . '/fonts/source-sans-pro/source-sans-pro.css', 
-        false
-    );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 ?>
